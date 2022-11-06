0319130ba7d67674e00c5503e992176fe5dc4a0e
unknown <sifatniloy18@gmail.com>
2022-11-06 11:00:03 -0600

    delete order api done

--- index.js
+++ index.js
@@ -84,6 +84,13 @@ async function run() {
             const result = await packageCollection.deleteOne(query);
             res.send(result);
         })
+        //delete order
+        app.delete("/order/:id", async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const result = await orderCollection.deleteOne(query);
+            res.send(result);
+        })
 
         //order collection api
         app.get('/order', async (req, res) => {
